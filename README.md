e2af99cd9d851b7c31cd346e8cb3ee4fdb8aab59
Akkuman <akkumans@qq.com>
2021-08-27 13:05:29 +0800

    :tada:

---
+++ README.md
@@ -0,0 +1,13 @@
+# github.dev-button
+
+github.dev 是 Github 官方的编辑查看 github 仓库的 Web IDE，和 Github1s 的功能差不多。
+
+该油猴插件用来在 github 仓库页面上显示一个打开 github.dev 的快捷方式
+
+## 脚本网址
+
+![显示 Github.dev 按钮](https://greasyfork.org/zh-CN/scripts/431463-%E6%98%BE%E7%A4%BA-github-dev-%E6%8C%89%E9%92%AE)
+
+## 致谢
+
+该脚本只是对 https://greasyfork.org/zh-CN/scripts/421631-%E6%98%BE%E7%A4%BA-github1s-%E6%8C%89%E9%92%AE 脚本的略微更改。
---
+++ github.dev.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         显示 Github.dev 按钮
+// @icon         https://github.com/favicons/favicon-codespaces.svg
+// @namespace    https://github.com/akkuman
+// @version      1.0.0
+// @description  在 Github 网站顶部显示 Github.dev 按钮，Github.dev 是一个利用 VsCode Online 浏览代码的项目
+// @author       Akkuman
+// @match        https://github.com/*
+// @grant        none
+// @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.slim.min.js
+// @reference    https://greasyfork.org/zh-CN/scripts/421631-%E6%98%BE%E7%A4%BA-github1s-%E6%8C%89%E9%92%AE
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    createButton();
+    $(document).on("pjax:end", ()=>{
+        createButton();
+    });
+})();
+
+/**
+ * 创建 Github.dev 按钮
+ */
+function createButton() {
+    const preSelectorButton = document
+        .querySelector("#githubdevButton");
+
+    if (preSelectorButton !== null) {
+        return;
+    }
+
+    const githubdevUrl = `https://Github.dev${location.pathname}`;
+    const element = '<li id="githubdevButton"> <a target="_blank" class="btn btn-sm" href="' + githubdevUrl + '"> <img class="icon octicon octicon-heart text-pink" src="https://github.com/favicons/favicon-codespaces.svg" />Github.dev </a> </li>';
+    const node = document.querySelector('.pagehead-actions.flex-shrink-0.d-none.d-md-inline');
+    if(node !== null){
+        node.insertAdjacentHTML('afterBegin', element);
+    }
+}
